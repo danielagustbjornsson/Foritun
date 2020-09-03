0db59b6b666da65aaca10a41ed81ac2391511b2a
Daníel Ágúst Björnsson <danielb20@ru.is>
2020-09-03 10:57:53 +0000

    finnal edit

---
+++ sequence.py
@@ -0,0 +1,25 @@
+# take the length of the sequence as a input and put the value in a int n
+
+# check if the n = 1
+    # set 1 into the int num_first
+# check if n = 2
+    # set 1 into the int num_first
+    # set 2 into the int num_second
+# check if n >= 3
+    # set 1 into the int num_first
+    # set 2 into the int num_second
+    # set 3 into the int num_third
+
+# start a for loop that ends when the value reaches the value of n using the conter i
+    # check if i = 1
+        # print num_first
+    # check if i = 2
+        # print num_second
+    # check if i = 3
+        # print num_third
+    # check if i > 3
+        # add num_first, num_second, num_third and put it into num_third_n
+        # num_fist = num_second
+        # num_second = num_third
+        # num_third = num_third_n
+        # print num_third
