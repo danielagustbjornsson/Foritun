d27190c9a3b04b9d51834262d22189763ef06f7a
Daníel Ágúst Björnsson <danielb20@ru.is>
2020-09-03 10:11:00 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,10 @@
+# set max_in as -1
+# start a loop
+    # tak in input and put it in num_int
+    # check if the max_int = -1 
+        # then set max_in as num_int (max_int = num_int)
+    # checking if the num_int is lower then zero
+        # exit the loop
+    # check if the num_int is bigger then max_int
+        # set max_int as num_int
+# write out the max_int 
